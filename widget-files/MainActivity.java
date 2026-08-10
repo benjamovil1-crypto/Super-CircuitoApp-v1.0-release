1f3bcb40ec8de43bb28ac92801ca36adb203d66a
benjamovil1-crypto <benjamovil1@gmail.com>
2026-08-09 20:34:02 -0600

    Add MainActivity to handle widget updates
    Implement MainActivity for widget management.

---
+++ widget-files/MainActivity.java
@@ -0,0 +1,46 @@
+package com.benjamovil.supercircuitoapp;
+
+import android.os.Bundle;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Intent;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        actualizarWidget();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        actualizarWidget();
+    }
+
+    private void actualizarWidget() {
+        try {
+            Intent intent = new Intent(getApplicationContext(), WidgetProvider.class);
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+
+            AppWidgetManager widgetManager = AppWidgetManager.getInstance(getApplicationContext());
+            ComponentName widgetComponent = new ComponentName(getApplicationContext(), WidgetProvider.class);
+            int[] widgetIds = widgetManager.getAppWidgetIds(widgetComponent);
+
+            if (widgetIds != null && widgetIds.length > 0) {
+                intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, widgetIds);
+                sendBroadcast(intent);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
